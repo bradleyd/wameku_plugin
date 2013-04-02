18f6e0ea7a8e222f73e80510457cfe3a279b8744
Brad Smith <bradleydsmith@gmail.com>
2013-04-02 10:12:58 -0400

    add more common tests

--- test/test_common.rb
+++ test/test_common.rb
@@ -2,17 +2,26 @@ require_relative 'test_helper'
 
 describe WamekuPlugin do
 
-  let(:sample_plugin) { 
-    class Foo
-      include WamekuPlugin::Common
-      def foo
-        "bar"
-      end
-    end
-  }
+  before  do
+   class Foo 
+     include WamekuPlugin::Common
+     def baz
+       "green"
+     end
+   end
+   @sample_plugin = Foo.new
+  end
+
   it "should repond to #run" do
-    p @sample_plugin
     assert_respond_to @sample_plugin, :run
+  end
+
+  it "should respond to #baz" do
+    assert_respond_to @sample_plugin, :baz
+  end
+
+  it "should load plugin's instance methods" do
+    assert_equal @sample_plugin.run, {baz: "green"}
   end
 
 end
